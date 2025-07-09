636a8f536a1b065749bca93fa3689578f2388abe
jmikhaelz <jmz06@outlook.com>
2025-07-08 20:06:36 -0600

    Create (project) : Creacion del proyecto de spring boot 3 y JDK 17

---
+++ src/test/java/mx/aluras/literatura/gutendex/GutendexApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.aluras.literatura.gutendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
