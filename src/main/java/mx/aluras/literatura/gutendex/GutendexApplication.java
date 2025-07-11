5b2fcd968fd1497e1aaac5c051b374226e18b4f2
jmikhaelz <jmz06@outlook.com>
2025-07-10 22:22:03 -0600

    update : Permisos de propiedades de archivo

--- src/main/java/mx/aluras/literatura/gutendex/GutendexApplication.java
+++ src/main/java/mx/aluras/literatura/gutendex/GutendexApplication.java
@@ -2,7 +2,9 @@ package mx.aluras.literatura.gutendex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class GutendexApplication {
 
